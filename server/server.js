f4414eb5863c75b64d4f712c73e541163a549337
anun namsudtho <namsudtho@outlook.com>
2018-06-17 12:45:06 +0700

    init server application for sing-song

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world!'
+  })
+})
+
+const port = process.env.PORT || 8081
+app.listen(port, () => console.log('Server started on port ' + port))
